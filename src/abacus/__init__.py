16544265a435ed9f35dcf90a5f8061d205d7b627
simondrue <simondrue@gmail.com>
2026-04-21 15:36:19 +0200

    Migrate project configuration from setup.py to pyproject.toml

--- src/abacus/__init__.py
+++ src/abacus/__init__.py
@@ -1,3 +1,6 @@
-from importlib.metadata import version
+from importlib.metadata import PackageNotFoundError, version
 
-__version__ = version("abacus")
+try:
+    __version__ = version("abacus")
+except PackageNotFoundError:
+    __version__ = "unknown"
